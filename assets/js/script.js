00f1b208e37103acc4c384e2e2b35995153bd4e1
Humberto Teixeira <humberto.quim@gmail.com>
2023-01-19 21:59:21 -0300

    Atualizações da aula 02
    Commit final da aula 02

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-const celular = {
-    cor: 'preto',
-    ligar: function() {
-        const mensagem = "ligando"
-        alert(mensagem)
-    }
+
+const form = document.querySelector('#form-habits') //querySelector é uma FUNÇÃO do OBJETO document
+
+//Criando e instanciando, ao mesmo tempo, o OBJETO nlwSetup
+const nlwSetup = new NLWSetup(form)
+
+//Criando OBJETO data
+const data = {
+    run: ['01-01', '01-02', '01-06', '01-07', '01-08'], //ARRAY
+    water: ['01-04', '01-05'], //ARRAY
+    food: ['01-01', '01-03'], //ARRAY
+    journal: ['01-02'],
+    takePills: ['01-03']
 }
 
+//FUNÇÃO .setData
+nlwSetup.setData(data)
 
-celular.ligar()
+//FUNÇÂO .load
+nlwSetup.load()
